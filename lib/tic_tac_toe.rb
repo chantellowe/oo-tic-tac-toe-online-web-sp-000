63f73c8995de933066caa69e893d9bac72c915dd
Chantel Lowe <chantellowe17@gmail.com>
2019-08-28 05:28:21 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -73,17 +73,15 @@ class TicTacToe
       end
   end
   
-  def won?(board) 
+  def won? 
     WIN_COMBINATIONS.detect do | win_combination |
-    
-    win_index_1 = win_combination[0] 
-    win_index_2 = win_combination[1] 
-    win_index_3 = win_combination[2] 
-    
-    board[win_index_1] == board[win_index_2] &&
-    board[win_index_2] == board[win_index_3] && 
-    board[win_index_1] != " " 
-    
+      win_index_1 = win_combination[0] 
+      win_index_2 = win_combination[1] 
+      win_index_3 = win_combination[2] 
+      
+      @board[win_index_1] == @board[win_index_2] &&
+      @board[win_index_2] == @board[win_index_3] && 
+      @board[win_index_1] != " " 
     end 
   end
   
